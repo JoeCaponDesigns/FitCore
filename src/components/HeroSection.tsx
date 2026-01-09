1c81980304f894b4da3979772384e41245a8f799
Joeeeeca <joechapon101@gmail.com>
2026-01-09 11:25:09 +0000

    Deleted the scroll down animation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -83,19 +83,7 @@ export default function HeroSection() {
         </div>
       </div>
 
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10">
-        <motion.div
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 2, repeat: Number.POSITIVE_INFINITY }}
-          className="w-6 h-10 rounded-full border-2 border-foreground/30 flex items-start justify-center p-2"
-        >
-          <motion.div
-            animate={{ y: [0, 12, 0] }}
-            transition={{ duration: 2, repeat: Number.POSITIVE_INFINITY }}
-            className="w-1 h-2 bg-foreground/30 rounded-full"
-          />
-        </motion.div>
-      </div>
+      
     </section>
   )
 }
